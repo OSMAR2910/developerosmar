e08170db9bc56844d5692d6ff2ce4ab7f7747e3c
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2023-01-09 21:59:02 -0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Animaciones
 window.addEventListener('scroll', function(){
     let animacion = document.getElementById("len_cont");
     let posicionObj1 = animacion.getBoundingClientRect().top;
@@ -34,3 +35,66 @@ window.addEventListener('scroll', function(){
         animacion.style.animation = "lef 2.5s ease-out"
     }
 })
+//Cursor
+function isTouchDevice() {
+	return (('ontouchstart' in window) ||
+			  (navigator.maxTouchPoints > 0) ||
+			  (navigator.msMaxTouchPoints > 0));
+}
+const isTouch = isTouchDevice();
+if (!isTouch){
+
+	const cursorEl = document.querySelector('.js-cursor');
+	const isClickedClass = 'is-clicked';
+	const isHiddenClass = 'is-hidden';
+	const isLinkHoveredClass = 'is-link-hovered';
+	const hasCustomCursorClass = 'has-custom-cursor';
+
+	const addEventListeners = () => {
+		document.addEventListener("mousemove", onMouseMove);
+		document.addEventListener("mousedown", onMouseDown);
+		document.addEventListener("mouseup", onMouseUp);
+		document.addEventListener("mouseenter", onMouseEnter);
+		document.addEventListener("mouseleave", onMouseLeave);
+		handleLinkHoverEvents();
+	};
+
+	const onMouseMove = (e) => {
+		cursorEl.style.setProperty('--cursor-x', e.clientX + "px");
+		cursorEl.style.setProperty('--cursor-y', e.clientY + "px");
+	};
+
+	const onMouseDown = () => {
+		cursorEl.classList.add(isClickedClass);
+	};
+
+	const onMouseUp = () => {
+		cursorEl.classList.remove(isClickedClass);
+	};
+
+	const onMouseEnter = () => {
+		cursorEl.classList.remove(isHiddenClass);
+	};
+
+	const onMouseLeave = () => {
+		cursorEl.classList.add(isHiddenClass);
+	};
+
+	const handleLinkHoverEvents = () => {
+		document.querySelectorAll('a, button, .js-link, input[type="button"], input[type="submit"]').forEach((el) => {
+			el.addEventListener("mouseover", () => cursorEl.classList.add(isLinkHoveredClass) );
+			el.addEventListener("mouseout", () => cursorEl.classList.remove(isLinkHoveredClass) );
+		});
+	};
+
+	addEventListeners();
+	document.body.classList.add(hasCustomCursorClass);
+
+	// DEBUG
+	var debugButton = document.querySelector('.js-debug-button');
+	debugButton.onclick = function() {
+		debugButton.classList.toggle('is-active');
+		document.body.classList.toggle('is-debug');
+	}
+
+}
