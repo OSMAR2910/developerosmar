8b1477a4ea13ba69995dbe73e90bd834645ab490
OSMAR SF <71989728+OSMAR2910@users.noreply.github.com>
2023-04-05 16:28:11 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,146 @@
-function isTouchDevice(){return"ontouchstart"in window||navigator.maxTouchPoints>0||navigator.msMaxTouchPoints>0}window.onload=function(){var e=document.getElementById("loader");e.style.visibility="hidden",e.style.opacity="0"};const isTouch=isTouchDevice();if(!isTouch){let e=document.querySelector(".js-cursor"),t="is-clicked",n="is-hidden",o="is-link-hovered",s=()=>{document.addEventListener("mousemove",i),document.addEventListener("mousedown",c),document.addEventListener("mouseup",a),document.addEventListener("mouseenter",l),document.addEventListener("mouseleave",r),d()},i=t=>{e.style.setProperty("--cursor-x",t.clientX+"px"),e.style.setProperty("--cursor-y",t.clientY+"px")},c=()=>{e.classList.add(t)},a=()=>{e.classList.remove(t)},l=()=>{e.classList.remove(n)},r=()=>{e.classList.add(n)},d=()=>{document.querySelectorAll('a, button, .js-link, input[type="button"], input[type="submit"]').forEach(t=>{t.addEventListener("mouseover",()=>e.classList.add(o)),t.addEventListener("mouseout",()=>e.classList.remove(o))})};s(),document.body.classList.add("has-custom-cursor")}const abrirmodal=document.querySelector("#button_cv_abrir"),cerrarmodal=document.querySelector("#button_cv_cerrar"),modal=document.querySelector("#modal");abrirmodal.addEventListener("click",()=>{modal.showModal()}),cerrarmodal.addEventListener("click",()=>{modal.close()}),window.addEventListener("scroll",function(){let e=document.getElementById("pro_cont"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){let e=document.getElementById("len_cont"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){let e=document.getElementById("tec_cont_cont1"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){let e=document.getElementById("tec_cont_cont2"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){let e=document.getElementById("res_cont_cont"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){let e=document.getElementById("con_cont"),t;e.getBoundingClientRect().top<window.innerHeight/1&&(e.style.animation="iz 2s ease-out")}),window.addEventListener("scroll",function(){var e=document.querySelector("header"),t=document.querySelector("#navbarNav");e.classList.toggle("abajo",window.scrollY>0),t.classList.toggle("abajo",window.scrollY>0)});var box=document.getElementById("in"),header=document.getElementById("header"),contador=0;function cambio(){0==contador?(box.classList.add("fullscreen"),header.classList.add("fullscreen"),contador=1):(box.classList.remove("fullscreen"),header.classList.remove("fullscreen"),contador=0)}btn1.addEventListener("click",cambio,!0),btn2.addEventListener("click",cambio,!0),btn3.addEventListener("click",cambio,!0),btn4.addEventListener("click",cambio,!0),btn5.addEventListener("click",cambio,!0),btn6.addEventListener("click",cambio,!0);const switchButton=document.getElementById("switch"),nav=document.querySelector("header");switchButton.addEventListener("click",()=>{document.body.classList.toggle("dark"),switchButton.classList.toggle("active"),nav.classList.toggle("dark")});
+//Crarga
+window.onload = function() {
+	var loader = document.getElementById('loader');
+	loader.style.visibility = 'hidden';
+	loader.style.opacity = '0';
+}
+//Cursor
+function isTouchDevice() {
+return(('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+}
+const isTouch = isTouchDevice();
+if(!isTouch) {
+const cursorEl = document.querySelector('.js-cursor');
+const isClickedClass = 'is-clicked';
+const isHiddenClass = 'is-hidden';
+const isLinkHoveredClass = 'is-link-hovered';
+const hasCustomCursorClass = 'has-custom-cursor';
+const addEventListeners = () => {
+	document.addEventListener("mousemove", onMouseMove);
+	document.addEventListener("mousedown", onMouseDown);
+	document.addEventListener("mouseup", onMouseUp);
+	document.addEventListener("mouseenter", onMouseEnter);
+	document.addEventListener("mouseleave", onMouseLeave);
+	handleLinkHoverEvents();
+};
+const onMouseMove = (e) => {
+	cursorEl.style.setProperty('--cursor-x', e.clientX + "px");
+	cursorEl.style.setProperty('--cursor-y', e.clientY + "px");
+};
+const onMouseDown = () => {
+	cursorEl.classList.add(isClickedClass);
+};
+const onMouseUp = () => {
+	cursorEl.classList.remove(isClickedClass);
+};
+const onMouseEnter = () => {
+	cursorEl.classList.remove(isHiddenClass);
+};
+const onMouseLeave = () => {
+	cursorEl.classList.add(isHiddenClass);
+};
+const handleLinkHoverEvents = () => {
+	document.querySelectorAll('a, button, .js-link, input[type="button"], input[type="submit"]').forEach((el) => {
+		el.addEventListener("mouseover", () => cursorEl.classList.add(isLinkHoveredClass));
+		el.addEventListener("mouseout", () => cursorEl.classList.remove(isLinkHoveredClass));
+	});
+};
+addEventListeners();
+document.body.classList.add(hasCustomCursorClass);
+}
+//Modal1
+const abrirmodal = document.querySelector("#button_cv_abrir");
+const cerrarmodal = document.querySelector("#button_cv_cerrar");
+const modal = document.querySelector("#modal");
+abrirmodal.addEventListener("click", () => {
+modal.showModal();
+})
+cerrarmodal.addEventListener("click", () => {
+	modal.close();
+})
+//Animaciones
+window.addEventListener('scroll', function() {
+let animacion = document.getElementById("pro_cont");
+let posicionObj1 = animacion.getBoundingClientRect().top;
+let tamañoDePantalla = window.innerHeight / 1;
+if(posicionObj1 < tamañoDePantalla) {
+	animacion.style.animation = "iz 2s ease-out"
+}
+})
+window.addEventListener('scroll', function() {
+let animacion = document.getElementById("len_cont");
+let posicionObj1 = animacion.getBoundingClientRect().top;
+let tamañoDePantalla = window.innerHeight / 1;
+if(posicionObj1 < tamañoDePantalla) {
+	animacion.style.animation = "iz 2s ease-out"
+}
+})
+window.addEventListener('scroll', function() {
+let animacion = document.getElementById("tec_cont_cont1");
+let posicionObj1 = animacion.getBoundingClientRect().top;
+let tamañoDePantalla = window.innerHeight / 1;
+if(posicionObj1 < tamañoDePantalla) {
+	animacion.style.animation = "iz 2s ease-out"
+}
+})
+window.addEventListener('scroll', function() {
+let animacion = document.getElementById("tec_cont_cont2");
+let posicionObj1 = animacion.getBoundingClientRect().top;
+let tamañoDePantalla = window.innerHeight / 1;
+if(posicionObj1 < tamañoDePantalla) {
+	animacion.style.animation = "iz 2s ease-out"
+}
+})
+window.addEventListener('scroll', function() {
+let animacion = document.getElementById("res_cont_cont");
+let posicionObj1 = animacion.getBoundingClientRect().top;
+let tamañoDePantalla = window.innerHeight / 1;
+if(posicionObj1 < tamañoDePantalla) {
+	animacion.style.animation = "iz 2s ease-out"
+}
+})
+window.addEventListener('scroll', function() {
+	let animacion = document.getElementById("con_cont");
+	let posicionObj1 = animacion.getBoundingClientRect().top;
+	let tamañoDePantalla = window.innerHeight / 1;
+	if(posicionObj1 < tamañoDePantalla) {
+		animacion.style.animation = "iz 2s ease-out"
+	}
+})
+//Aniscroll nav
+window.addEventListener("scroll", function() {
+	var header = document.querySelector("header");
+	var navbarNav = document.querySelector("#navbarNav");
+	header.classList.toggle("abajo", window.scrollY > 0);
+	navbarNav.classList.toggle("abajo", window.scrollY > 0);
+})
+//fullscreen
+var box = document.getElementById('in'),
+header = document.getElementById('header'),
+contador = 0;
+
+function cambio() {
+if(contador == 0) {
+	box.classList.add('fullscreen');
+	header.classList.add('fullscreen');
+	contador = 1;
+} else {
+	box.classList.remove('fullscreen');
+	header.classList.remove('fullscreen');
+	contador = 0;
+}
+}
+btn1.addEventListener('click', cambio, true);
+btn2.addEventListener('click', cambio, true);
+btn3.addEventListener('click', cambio, true);
+btn4.addEventListener('click', cambio, true);
+btn5.addEventListener('click', cambio, true);
+btn6.addEventListener('click', cambio, true);
+//Modo oscuro
+const switchButton = document.getElementById('switch');
+const nav = document.querySelector("header");
+switchButton.addEventListener('click', () => {
+document.body.classList.toggle('dark');
+switchButton.classList.toggle('active');
+nav.classList.toggle('dark');
+});
